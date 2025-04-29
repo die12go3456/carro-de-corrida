8ca1f3fd1a46246801103757252833f8016e55b8
die12go3456 <diego.valencio@escola.pr.gov.br>
2025-04-29 11:53:43 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,58 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+let xJogador = [0, 0, 0, 0];
+let yJogador = [75, 150, 225, 300];
+let jogador = ["🦖​", "🐧​", "​🐉", ];
+let teclas = ["a", "s", "d", "f​"];
+let quantidade = jogador.length;
+
+function draw() {
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+  verificaVencedor();
+}
+
+function ativaJogo() {
+  if (focused == true) {
+    background("#001EFF");
+  } else {
+    background("#0033FF");
+  }
+}
+
+function desenhaJogadores() {
+  textSize(40);
+  for (let i = 0; i < quantidade; i++) {
+    text(jogador[i], xJogador[i], yJogador[i]);
+  }
+}
+
+function desenhaLinhaDeChegada() {
+  fill("white");
+  rect(350, 0, 10, 400);
+  fill("red");
+  for (let yAtual = 0; yAtual < 400; yAtual += 20) {
+    rect(350, yAtual, 10, 10);
+  }
+}
+
+function verificaVencedor() {
+  for (let i = 0; i < quantidade; i++) {
+    if (xJogador[i] > 350) {
+      text(jogador[i] + " venceu!", 50, 200);
+      noLoop();
+    }
+  }
+}
+
+function keyReleased() {
+  for (let i = 0; i < quantidade; i++) {
+    if (key == teclas[i]) {
+      xJogador[i] += random(20);
+    }
+  }
+}
+as
